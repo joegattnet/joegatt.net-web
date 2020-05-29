e97167fecddaefc8baab88f0018f894df4da4157
Joe Gatt <joe@joegatt.net>
2020-05-29 13:39:57 +0200

    Fix body text

---
+++ src/components/Body.js
@@ -0,0 +1,172 @@
+import React, { Component, createRef } from 'react'
+import parse, { domToReact } from 'html-react-parser';
+import styled from 'styled-components';
+
+import {
+  ANNOTATIONS_FONT_SIZE,
+  ANNOTATIONS_LINE_HEIGHT,
+  DESKTOP_BREAKPOINT,
+  FONT_SIZE,
+  HALF_TAB,
+  RED,
+  TAB
+} from '../variables';
+
+const StyedBody = styled.section`
+  display: grid;
+  grid-template-columns: 2fr 1fr 1fr;
+  grid-template-rows: auto;
+  grid-column-gap: ${TAB};
+  .body {
+    font-family: 'Roboto Slab', serif;
+    font-weight: 400;
+    position: relative;
+    grid-column: 1 / span 1;
+    grid-row: 1 / span 1;
+  }
+  p {
+    display: block;
+    font-size: ${FONT_SIZE};
+    hyphens: auto;
+    margin: 0;
+    overflow: hidden;
+    ${'' /* position: relative; */}
+    text-align: justify;
+    text-indent: ${TAB};
+    &:first-of-type {
+      text-indent: 0;
+    }
+    a.annotation-mark {
+      font-weight: 400;
+      font-size: 9px;
+      line-height: 0;
+      vertical-align: super;
+      &.squash {
+        margin-left: -2px;
+      }
+    }
+  }
+  #annotations {
+    font-family: 'Roboto Slab', serif;
+    font-size: 14px;
+    font-weight: 300;
+    grid-row: 2 / span 1;
+    @media screen and (min-width: ${DESKTOP_BREAKPOINT}) {
+      grid-column: 2 / span 1;
+      grid-row: 1 / span 1;
+      position: relative;
+      ol {
+        padding: 0;
+      }
+      li {
+        color: ${RED};
+        line-height: ${ANNOTATIONS_LINE_HEIGHT};
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        position: absolute;
+        text-align: left;
+        font-size: ${ANNOTATIONS_FONT_SIZE};
+        font-weight: 400;
+        a:first-of-type {
+          color: ${RED};
+          display: inline-block;
+          min-width: ${HALF_TAB};
+          &:after {
+            content: '. ';
+          }
+        }
+      }
+    }
+  }
+  #versions {
+    ol {
+      padding: 0;
+    }
+    li {
+      color: ${RED};
+      line-height: ${ANNOTATIONS_LINE_HEIGHT};
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      position: absolute;
+      text-align: left;
+      font-size: ${ANNOTATIONS_FONT_SIZE};
+      a:first-of-type {
+        color: ${RED};
+        display: inline-block;
+        min-width: ${HALF_TAB};
+        &:before {
+          content: 'v';
+        }
+      }
+    }
+  }
+`;
+
+class Body extends Component {
+  constructor(props) {
+    super(props);
+    this.bodyContainer = createRef();
+    this.annotationMarks = [];
+    this.annotations = [];
+    for (let i = 0; i < props.annotationsCount; i++) {
+      this.annotationMarks[i] = createRef();
+      this.annotations[i] = createRef();
+    }
+  }
+
+  componentDidMount() {
+    let minimum = 0;
+    let newTop = minimum;
+    let correctedTop = minimum;
+    this.annotationMarks.forEach((annotationMark, index) => {
+      newTop = annotationMark.current.offsetTop;
+      correctedTop = (newTop <= minimum ? minimum : newTop);
+      console.log(correctedTop);
+      this.annotations[index].current.style.top = `${correctedTop}px`;
+      minimum = correctedTop + this.annotations[index].current.offsetHeight;
+    });
+
+    const reversedAnnotations = this.annotations.reverse();
+    let maximum = this.bodyContainer.current.offsetHeight;
+    let newBottom;
+
+    reversedAnnotations.forEach((annotation, index) => {
+      newTop = annotation.current.offsetTop;
+      newBottom = newTop + annotation.current.offsetHeight;
+      correctedTop = newBottom > maximum ? maximum - annotation.current.offsetHeight : newTop;
+      this.annotations[index].current.style.top = `${correctedTop}px`;
+      maximum = correctedTop;
+    });
+  }
+
+  render() {
+    const { cachedBodyHtml } = this.props;
+    const options = { replace:
+      ({ attribs, children }) => {
+        if (!attribs) return;
+        if (attribs.class && attribs.class.includes('annotation-mark')) {
+          const index = parseInt(children[0].data, 10);
+          return (
+            <a class={attribs.class} href={`#annotation-${ index }`} ref={this.annotationMarks[index - 1]} id={`annotation-mark-${ index }`}>
+              {domToReact(children, options)}
+            </a>
+          );
+        }
+        if (attribs.class === 'annotations-container') {
+          return (
+              <ol>{children.map((item, index) => (
+                <li id={`annotation-${ index + 1 }`} ref={this.annotations[index]}>
+                <a href={`#annotation-${ index + 1 }`}>{index + 1}</a>{item.children[0].data}
+              </li>
+            ))}</ol>
+          );
+        }
+      }};
+  
+    return <StyedBody ref={this.bodyContainer}>{parse(cachedBodyHtml, options)}</StyedBody>;
+  }
+}
+
+export default Body;
--- src/pages/Text.js
+++ src/pages/Text.js
@@ -1,26 +1,23 @@
-import React, { useRef } from 'react'
-// import { Chart } from 'react-google-charts';
-// import { Link } from 'react-router-dom';
+import React, { useRef, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 import { useQuery } from 'react-apollo-hooks';
-// import { UserContext } from '../helpers/UserContext';
 import gql from 'graphql-tag';
-import parse from 'html-react-parser';
+
 
 import Header from '../components/Header';
-import Text from '../components/Text';
+import Body from '../components/Body';
 
 const TEXT_QUERY = gql`
   query TextsQuery($textId: Int) {
     text(uid: $textId) {
       id
-      body
+      annotationsCount
       cachedBodyHtml
       cachedHeadline
       cachedSubheadline
       distance
       introduction
       wordCount
-      annotationsCount
     }
   }
 `;
@@ -29,26 +26,14 @@ export default ({ match }) => {
   const { loading, error, data, } = useQuery(TEXT_QUERY, { variables: { textId: parseInt(match.params.id, 10) } });
   if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`;
-  // const annotationsContainer = new Array(data.text.annotationsCount);
-  // if (!!data.text.annotationsCount) {
-    let annotationMarks = new Array(data.text.annotationsCount).map(item => item = useRef());
-  // }
+
   return (
     <article>
-      <h1>{data.text.annotationsCount} || {annotationMarks.length}</h1>
       <Header
         headline={data.text.cachedHeadline}
         subheadline={data.text.cachedSubheadline}
       />
-      {/* <Text dangerouslySetInnerHTML={{ __html: data.text.cachedBodyHtml }} /> */}
-      <Text>{parse(data.text.cachedBodyHtml)}</Text>
-      {/* <Text>
-        <section className="body">
-          <section>
-          <p>On this site, whenev~er the code is re~ferred to, as in hin~ter~land, there are no links ex~plain~ing the terms. No “What is Rails?” or “What is a serv~er?” link. This is not a pop~u~lar sci~ence pro~ject. I don’t over-ex~plain the prose so why ex~plain this?<a href="#annotation-1" ref={annotationMarks[0]} id="annotation-mark-1">1</a> Elab~or~at~ing these terms and then not elab~or~at~ing on lit~er~ary terms would sug~gest a pre~sumed kind of read~er. (Then again, the spir~it of the in~ter~net is that everything is ex~plained down to the low~est—“highest”—level. Even the most ar~cane tech~nic~al dis~cus~sions provide links that would cas~cade to an in~tro~duc~tion for ab~so~lute be~gin~ners.) An un~in~ten~ded con~sequence of this may be that the sec~tion comes across as though it’s meant to dazzle the read~er.<a class="squash" href="#annotation-2" ref={annotationMarks[1]} id="annotation-mark-2">2</a></p>
-          </section>
-        </section>
-      </Text> */}
+      <Body cachedBodyHtml={data.text.cachedBodyHtml} annotationsCount={data.text.annotationsCount} />
 
       {/* <Chart
         chartType="AreaChart"
